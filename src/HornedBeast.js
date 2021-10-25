c9660c6de27dc4ec71cad48fea9403f40d85340f
Joe Streifel <josephstreifel@gmail.com>
2021-10-25 14:19:17 -0700

    adds necessary components

---
+++ src/HornedBeast.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class HornedBeast extends Component {
+  render() {
+    return (
+      <>
+        <h2></h2>
+        <img></img>
+        <p></p>
+      </>
+    )
+  }
+}
+
+export default HornedBeast;
