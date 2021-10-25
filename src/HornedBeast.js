b41842e09844024cf75a1896cdf12f5495fa1c81
Joe Streifel <josephstreifel@gmail.com>
2021-10-25 15:55:02 -0700

    adds beast images and renders to page

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 
+
 class HornedBeast extends Component {
   render() {
     return (
       <>
-        <h2></h2>
-        <img></img>
-        <p></p>
+        <h2>{this.props.title}</h2>
+        <img src={this.props.imageUrl} alt='Horned beast' title='Look at those horns!' />
+        <p>{this.props.description}</p>
       </>
     )
   }
--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
+import HornedBeast from './HornedBeast';
 
 class Main extends Component{
   render(){
     return(
-
+      <>
+        <HornedBeast title='Beast1' imageUrl='images/beast-sprite.png' description='A picture of the first beast' />
+        <HornedBeast title='Beast2' imageUrl='images/beast-sprite.png' description='A picture of the second beast' />
+      </>
     )
   }
 }
