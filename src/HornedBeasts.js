513f1c2435f9b5e62344df4adee120bc2ac2893c
Joe Streifel <josephstreifel@gmail.com>
2021-10-26 16:51:41 -0700

    adds ability to track click images and tracks times favorited

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
+import heart from './assets/heart-icon.png'
 
 
 class HornedBeasts extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      likes: 0
+    }
+  }
+
+  handleFav = () => (this.setState((state) => ({
+    likes: state.likes + 1
+  })))
+
   render() {
     return (
       <div id={this.props.title}>
         <h2>{this.props.title}</h2>
-        <img src={this.props.imageUrl} alt={this.props.keyword} title={this.props.title} />
+        <img src={this.props.imageUrl} alt={this.props.keyword} title={this.props.title} onClick={this.handleFav} />
         <p>{this.props.description}</p>
+        <span><img src={heart} alt='favorites' />Favorites: {this.state.likes}</span>
       </div>
     )
   }
